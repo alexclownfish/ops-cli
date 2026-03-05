861593284a44e49bf7224decb4e05855a181b8df
alexclownfish <ywz0207@163.com>
2026-03-05 17:31:56 +0800

    这是一个AI写的，也不知道能写成啥样

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ops-cli
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,20 @@
+﻿package main
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ops",
+	Short: "运维工具集 - 整合常用运维功能",
+	Long:  `ops-cli: 一个集成了服务器管理、监控、部署、日志分析等功能的运维工具集`,
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
